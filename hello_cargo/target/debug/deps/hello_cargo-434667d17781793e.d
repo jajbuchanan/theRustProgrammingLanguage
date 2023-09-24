125c93dbb65d12eaeafae7b091f09241099118a6
Jonathan Buchanan <axtract@gmail.com>
2023-09-24 16:39:52 +0000

    add subfolders to repo

---
+++ hello_cargo/target/debug/deps/hello_cargo-434667d17781793e.d
@@ -0,0 +1,5 @@
+/home/ubuntu/projects/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e: src/main.rs
+
+/home/ubuntu/projects/hello_cargo/target/debug/deps/hello_cargo-434667d17781793e.d: src/main.rs
+
+src/main.rs:
