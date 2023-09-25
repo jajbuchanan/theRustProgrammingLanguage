6399b67a9f58f106b3ce8f7f3c017c3558bcc323
Jonathan Buchanan <axtract@gmail.com>
2023-09-25 21:57:40 +0000

    new main file to show inner and outer scopes

---
+++ variables/main.bak
@@ -0,0 +1,9 @@
+fn main() {
+    // let x = 5;  // Rust variables are not mutable by default, meaning that this would throw an
+    // error; the x = 6 later on cannot change the immutable value
+    let mut x = 5; // This works because mut makes the variable mutable
+    println!("The value of x is: {x}"); 
+    x = 6; 
+    println!("The value of x is: {x}"); 
+    // Ultimately it is up to you to determine if mutability should be used in a given situation
+}
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,14 @@
+
+
 fn main() {
-    // let x = 5;  // Rust variables are not mutable by default, meaning that this would throw an
-    // error; the x = 6 later on cannot change the immutable value
-    let mut x = 5; // This works because mut makes the variable mutable
-    println!("The value of x is: {x}"); 
-    x = 6; 
-    println!("The value of x is: {x}"); 
-    // Ultimately it is up to you to determine if mutability should be used in a given situation
+    let x = 5; 
+
+    let x = x + 1; 
+
+    {
+        let x = x * 2; 
+        println!("The value of x in the inner scope is: {x}"); 
+    }
+
+    println!("The value of x in the outer scope is: {x}"); 
 }
